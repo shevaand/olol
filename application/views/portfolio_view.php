862f53fc42f10c24d98614f9dfa1ad5984d33bdb
shevaand <38583864+shevaand@users.noreply.github.com>
2019-07-22 18:09:58 +0300

    Update portfolio_view.php

--- application/views/portfolio_view.php
+++ application/views/portfolio_view.php
@@ -7,7 +7,7 @@
 
 	foreach($data as $row)
 	{
-		echo '<tr><td>'.'2018'.'</td><td>'.$row['Site'].'</td><td>'.$row['Description'].'</td></tr>';
+		echo '<tr><td>'.$row['Year'].'</td><td>'.$row['Site'].'</td><td>'.$row['Description'].'</td></tr>';
 	}
 	
 ?>

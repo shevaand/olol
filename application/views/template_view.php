56e32da9ce730813fee5a7f0f31166aa634d3f2c
shevaand <38583864+shevaand@users.noreply.github.com>
2019-07-22 18:12:40 +0300

    Update template_view.php

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
-		<title>ОЛОЛОША TEAM</title>
+		<title>Teste</title>
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
 		<link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" type="text/css" href="/css/style.css" />

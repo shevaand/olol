dbebedf95cd4ec3b912e77d6941ba16bb5cab60a
shevaand <bartsimpsonpvp@Gmail.com>
2019-07-22 18:07:59 +0300

    fi

---
+++ application/views/contacts_view.php
@@ -0,0 +1,10 @@
+<h1>Контакты</h1>
+<p>
+icq: 123123123123<br/>
+skypeid: skypeid123123123<br/>
+email: test123123@gmail.com<br/>
+<br/>
+<iframe align="left" src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0%2C%20%D0%A5%D0%BC%D0%B5%D0%BB%D1%8C%D0%BD%D0%B8%D1%86%D0%BA%D0%B0%D1%8F%20%D0%BE%D0%B1%D0%BB.+(%D0%A8%D0%B5%D0%BF%D0%B5%D1%82%D0%BE%D0%B2%D0%BA%D0%B0)&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed"
+frameborder="0" marginwidth="0" marginheight="0" scrolling="no" align="right" width="450" height="380">
+</iframe>
+</p>
